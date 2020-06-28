8a43fe44a03e3725259870b811f9133d0723f0fb
hpolatyuruk <h.polatyuruk@gmail.com>
2020-06-28 13:21:03 +0200

    -minor changes on index.ts

--- index.ts
+++ index.ts
@@ -3,22 +3,3 @@ import { UserPreferences } from "./models/user-preferences.ts";
 import { UserPreferencesRepository } from "./repositories/user-preferences.repository.ts";
 
 console.log(`App is running on port ${Deno.env.get("APP_PORT")}`);
-
-const preferences = new UserPreferences();
-preferences.pocketUserName = "hpolatyuruk";
-preferences.accessToken = "asd";
-preferences.cronExpression = "* * * * *";
-preferences.emailAddress = "huseyin@hey.com";
-preferences.linkCountPerDigest = 10;
-preferences.subscribed = true;
-
-const userPreferencesRepository = new UserPreferencesRepository();
-//await userPreferencesRepository.create(preferences);
-//preferences.pocketUserName = "amina";
-//await userPreferencesRepository.create(preferences);
-//preferences.emailAddress = "h.polatyuruk@gmail.com";
-//await userPreferencesRepository.update(preferences);
-
-const pref = await userPreferencesRepository.get("hpolatyuruk");
-console.log(pref);
-console.log(await userPreferencesRepository.getAll());
